0caa6574f81ceef9416f778a0c1c6b820b73db1e
sumit bansal <sumit.bansal@loconav.com>
2019-03-10 13:05:30 +0530

    changes done for user

--- app/controllers/user/base_controller.rb
+++ app/controllers/user/base_controller.rb
@@ -1,20 +1,2 @@
 class User::BaseController < ApplicationController
-
-  def show
-
-  end
-
-  def new
-    @user = User.new
-  end
-
-  def create
-    debugger
-    @user = User.find(params[:id])
-    if @user.save
-
-    else
-
-    end
-  end
 end
